c61af26348b775d642818b7ab0d3dc110ee6d823
Atanu Majumder <atanumajumder2004@gmail.com>
2023-08-24 12:22:52 +0530

    script file added

---
+++ script.js
@@ -0,0 +1,30 @@
+let countDownTime = new Date("Oct 20, 2023 00:00:00").getTime();
+
+const durgaPuja = () => {
+  let nowTime = new Date().getTime();
+  let diff = countDownTime - nowTime;
+
+  let sec = 1000;
+  let min = sec * 60;
+  let hr = min * 60;
+  let day = hr * 24;
+
+  let d = Math.floor(diff / day);
+  let h = Math.floor((diff % day) / hr);
+  let m = Math.floor((diff % hr) / min);
+  let s = Math.floor((diff % min) / sec);
+
+  document.getElementById("Days").innerText = addZero(d);
+  document.getElementById("Hours").innerText = addZero(h);
+  document.getElementById("Minutes").innerText = addZero(m);
+  document.getElementById("Seconds").innerText = addZero(s);
+};
+
+setInterval(function () {
+  durgaPuja();
+}, 1000);
+
+function addZero(num) {
+  if (num < 10) return `0${num}`;
+  else return num;
+}
